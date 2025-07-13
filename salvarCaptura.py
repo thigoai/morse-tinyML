4a7f678dd915a5468b4cfb0bfef89de0d15283ed
thigoai <tfreire352@gmail.com>
2025-07-13 19:22:31 -0300

    melhorando modelo

--- salvarCaptura.py
+++ salvarCaptura.py
@@ -44,8 +44,6 @@ with serial.Serial(porta, baud_rate, timeout=1) as ser, open(arquivo_saida, modo
                     linha_csv = ",".join(map(str, duracoes)) + f",{letra_atual}\n"
                     f.write(linha_csv)
                     duracoes = []
-
-            # Tecla especial para mudar a letra durante a execução
             if ser.in_waiting:
                 comando = input("Digite nova letra, ou pressione Enter para continuar com '" + letra_atual + "': ").strip().upper()
                 if comando:
